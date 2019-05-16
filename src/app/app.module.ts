8f7f897214b02726be42c14f576099b14507e4e7
sureshak07 <suresh.ak@iinerds.com>
2019-05-16 18:30:36 +0530

    component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './core/components/home/home.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { AppointmentComponent } from './layouts/appointment/appointment.component';
+import { LoginComponent } from './layouts/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    AppointmentComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
