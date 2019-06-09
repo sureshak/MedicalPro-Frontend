527626ec99d5648eb77893189992c2089ed8d38c
abinayagomathi <abinaya.iyer93@gmail.com>
2019-06-09 21:37:53 +0530

    porgress agm module import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AgmCoreModule } from '@agm/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './core/components/home/home.component';
